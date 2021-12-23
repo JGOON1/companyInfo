f062b383ab6f6128a237e68ede9debd518ec886e
unknown <john.meng014@gmail.com>
2021-12-23 14:53:55 -0500

    edit routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StockComponent } from './stock/stock.component';
 import { StocksComponent } from './stocks/stocks.component';
-// import { EditstockComponent } from './editstock/editstock.component';
+import { EditstockComponent } from './editstock/editstock.component';
 
 
 
 const routes: Routes = [
   {path:"stocks", component:StocksComponent},
   {path:"stocks/:id", component:StockComponent},
-  // {path:"stocks/:id/edit", component:EditstockComponent}
+  {path:"stocks/:id/edit", component:EditstockComponent}
 ];
 
 @NgModule({
